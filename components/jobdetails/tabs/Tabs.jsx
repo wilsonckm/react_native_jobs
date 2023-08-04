e3f69c54a2e86c564ee57b5bc99572d13147bb1e
wilsonckm <wilsonckmchan@gmail.com>
2023-08-04 16:10:53 -0400

    Commit 5

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, TouchableOpacity, FlatList } from "react-native";
 
-import styles from './tabs.style'
+import styles from "./tabs.style";
+import { SIZES } from "../../../constants";
 
-const Tabs = () => {
+const TabButton = ({ name, activeTab, onHandleSearchType }) => (
+  <TouchableOpacity>
+    <Text>{name}</Text>
+  </TouchableOpacity>
+);
+
+const Tabs = ({ tabs, activeTab, setActiveTab }) => {
   return (
-    <View>
-      <Text>Tabs</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={tabs}
+        renderItem={({ item }) => (
+          <TabButton
+            name={item}
+            activeTab={activeTab}
+            onHandleSearchType={() => setActiveTab(item)}
+          />
+        )}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Tabs
+export default Tabs;
